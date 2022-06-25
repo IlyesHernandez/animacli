abf4851980bb2de799cfeb7ee45bc29d170c5022
IlyesHernandez <ilyeshernandez.pro@gmail.com>
2022-06-25 22:08:04 +0200

    🚫 - Error on <dirname>/src/commands/help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,24 +1,19 @@
 /*
  * File Structure:
   * Import module we must to have.
-  * The Class.
-  * Export the Class.
+  * Export function
 */
 
-const { Command } = require("../class/Command.js");
-const { _commands } = require("../data/commands.js");
 const _style = require("../configuration/chalk.preset.js")
+const { _commands } = require("../data/commands.js")
 
-class Help extends Command {
-    constructor(){
-        super("help", "A simple pannel for help you to how to use commands.", "help", () => {
-            console.log(_style.title("Help Pannel"));
-            _commands.forEach((_command) => {
-                console.log(`How to use the command ('<name>' is a require setting but '<name?>' is not a require setting): ${_command.utility}`);
-                console.log(`Command Description: ${_command.description}`)
-            })
-        })
-    }
+module.exports = () => {
+    console.log(_style.title("Help Pannel"));
+    console.log(_commands);
+    _commands.forEach((_command) => {
+        console.log(chalk.cyan(`${_command.query}`))
+        console.log(` * How to use (<name> is a require setting, but, <name?> is a unrequire setting): ${_command.utility}`);
+        console.log(` * Description: ${_command.description}`);
+        console.log(); // space before new command
+    })
 }
-
-module.exports = new Help();
--- src/data/commands.js
+++ src/data/commands.js
@@ -4,19 +4,13 @@
   * Exports all commands.
 */
 
-const Help = require("../commands/help.js")
+const help = require("../commands/help.js")
 
 module.exports._commands = [
     {
-        query: Help.getQuery(),
-        description: Help.getDescription(),
-        utility: Help.getUtility(),
-        action: Help.getAction(),
-    },
-    {
-        query: "Help.getQuery()",
-        description: "Help.getDescription()",
-        utility: "Help.getUtility()",
-        action: "Help.getAction()",
+        query: "help",
+        description: "A simple pannel for all know about command.",
+        utility: "help",
+        action: help()
     },
 ]
